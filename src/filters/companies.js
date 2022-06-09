85e23a636f8c680890574caf20e240c3acae8bb3
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-09 15:17:03 -0700

    fixed commans in compaines

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -2,7 +2,7 @@ import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupCompanies = (store) => {
-    let companies = ['all', ...new Set(store.map((product) => product.company))]
+    let companies = ["all", ...new Set(store.map((product) => product.company))]
     const companiesDOM = getElement(".companies")
     companiesDOM.innerHTML = companies
         .map((company) => {
